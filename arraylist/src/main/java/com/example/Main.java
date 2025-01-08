5d349863ae2f0f8716dbb47b0053dfaa493cc891
Maike Miller <maikemiler@hotmail.com>
2025-01-08 12:34:14 -0300

    .

--- arraylist/src/main/java/com/example/Main.java
+++ arraylist/src/main/java/com/example/Main.java
@@ -3,5 +3,8 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bem vindo ao cadastro de nomes");
+
+        double numero1;
+        
     }
 }
